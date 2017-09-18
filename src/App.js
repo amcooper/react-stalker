99819d9fca6f01d5968a42a2882e756a1729ba76
Adam Cooper <amcooper@gmail.com>
2017-09-18 01:55:21 -0400

    Basic skeleton

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Form {
+  return (
+    <div>
+      FORM
+    </div>
+  )
+}
+
+function Stalk(props) {
+  return (
+    <div>
+      STALK {this.props.id}
+    </div>
+  )
+}
+
+function StalkList {
+  return (
+    <div>
+      STALKLIST
+    </div>
+  )
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: true,
+      id: -1
+    }
+  }
+  		
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form />
+        <div>
+	  this.state.list ? <StalkList /> : <Stalk id=this.state.id />
+	</div>
       </div>
     );
   }
