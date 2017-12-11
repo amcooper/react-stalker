6b350808e94d30feb69eafd9a283e762b5cb7bab
Adam Cooper <amcooper@gmail.com>
2017-12-10 20:47:39 -0500

    Fixed loose ends: updating reverts form to empty creation form, months saved from off-by-one error, debugging print statements removed, old code removed.

--- src/App.js
+++ src/App.js
@@ -23,20 +23,8 @@ class Form extends Component {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    console.info( 'handleChange', name, value );
-    this.setState({
-      [name]: value
-    });
+    this.setState({ [name]: value });
   }
-
-  // handleChange( event ) {
-  //   var partialState = {};
-  //   partialState.formValues = {};
-  //   partialState.formValues[ event.target.name ] = event.target.value;
-  //   console.info( 'handleChange:', event.target.name, event.target.value );
-  //   this.setState({ partialState });
-  //   console.info( 'formValues', this.state.formValues );
-  // }
 
   handleSubmit(event) {
     let data = new URLSearchParams();
@@ -45,18 +33,10 @@ class Form extends Component {
     data.append('date', this.state.date);
     data.append('location', this.state.location);
     data.append('comment', this.state.comment);
-    console.info(data.toString()); // debug
 
     event.preventDefault();
-    console.log('Handle submit here'); // debug
 
     let fetchURL = config.apiURL + ( this.props.isEditForm ? `/${this.props.item ? this.props.item.id : ''}` : '' );
-    console.info( 'config' );
-    console.info( config );
-    console.info( 'fetchURL', fetchURL );
-    // if ( this.props.isEditForm ) {
-    //   fetchURL += 
-    // }
     let fetchMethod = this.props.isEditForm ? 'PUT' : 'POST';
 
     fetch(fetchURL, {
@@ -70,6 +50,7 @@ class Form extends Component {
     })
     .then((res) => {
       this.resetForm();
+      this.props.resetAppState();
       this.props.getSightings();
       return res.json();
     })
@@ -77,10 +58,11 @@ class Form extends Component {
   }
 
   resetForm() {
+    let date = new Date( Date.now());
     this.setState({
       celebrity: '',
       stalker: '',
-      date: '',
+      date: `${ date.toISOString().slice( 0, -1 )}`,
       location: '',
       comment: ''
     });
@@ -89,7 +71,6 @@ class Form extends Component {
   componentWillReceiveProps(nextProps) {
     if ( nextProps.item ) {
       this.setState({
-      // set something
         celebrity: nextProps.item.celebrity || '',
         stalker: nextProps.item.stalker || '',
         date: nextProps.item.date.toISOString().slice( 0, -1 ) || '',
@@ -100,8 +81,6 @@ class Form extends Component {
   }
 
   render() {
-    console.info( 'render Form', this.props.item, this.state ); // debug
-
     return (
       <form id='input-form' onSubmit={this.handleSubmit}>
         <label>
@@ -172,10 +151,18 @@ class App extends Component {
       id: null,
       sightings: []
     }
+    this.resetAppState = this.resetAppState.bind( this );
     this.getSightings = this.getSightings.bind( this );
     this.handleClick = this.handleClick.bind( this );
     this.deleteItem = this.deleteItem.bind( this );
     this.editItem = this.editItem.bind( this );
+  }
+
+  resetAppState() {
+    this.setState({
+      id: null,
+      isEditForm: false
+    });
   }
 
   getSightings() {
@@ -189,11 +176,9 @@ class App extends Component {
         return res.json()
       })
       .then(( json ) => {
-        console.log('GET req'); // debug
-        console.info( 'json', json );
         let sightings = json.map(( sighting ) => {
           let [y,mo,d,h,min] = sighting.date.split(/[-T:Z]/); 
-          sighting.date = new Date(y, mo, d, h, min);
+          sighting.date = new Date(y, mo - 1, d, h, min);
           return sighting;
         });
         return sightings;
@@ -205,15 +190,11 @@ class App extends Component {
   }
 
   handleClick( id ) {
-    console.info('handleClick', id);
     this.setState({ id: id });
   }
 
   editItem( id ) {
-    this.setState({
-      // id: id,
-      isEditForm: true
-    });
+    this.setState({ isEditForm: true });
   }
 
   deleteItem( id ) {
@@ -228,10 +209,8 @@ class App extends Component {
         }
         this.setState({ id: null });
         this.getSightings();
-        // return res.json();
       })
       .catch( e => console.error( e.stack ));
-      // .then(( res ))
   }
 
   componentDidMount() {
@@ -239,15 +218,13 @@ class App extends Component {
   }
 
   render() {
-    console.info( 'render App' ); // debug
-
     let item = this.state.sightings.find((sighting) =>
-          sighting.id === this.state.id
-        );
+      sighting.id === this.state.id
+    );
 
     return (
       <div className="App">
-        <Form getSightings={this.getSightings} isEditForm={this.state.isEditForm} item={ this.state.isEditForm ? item : null} />
+        <Form resetAppState={this.resetAppState} getSightings={this.getSightings} isEditForm={this.state.isEditForm} item={ this.state.isEditForm ? item : null} />
         { item !== undefined && <Stalk item={ item } onEdit={( id ) => this.editItem( id )} onDelete={( id ) => this.deleteItem( id )} /> }
         <StalkList sightings={this.state.sightings} onClick={( id ) => this.handleClick( id )} />
       </div>
