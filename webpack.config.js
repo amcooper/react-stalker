9760046122f0f47a462addcfd178feb42b642132
Adam Cooper <theadamcooper@protonmail.com>
2018-08-28 04:38:47 -0400

    Webpack mostly working?

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./client/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "client", "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  }
+}
