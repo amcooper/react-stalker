dfd7786908310a176d8ea30def0155af237820de
Adam Cooper <amcooper@gmail.com>
2018-09-09 13:14:08 -0400

    Weird dos-a-dos between database.js and knexfile.js. Should stick to latter. More tests.

--- server/test/routes.spec.js
+++ server/test/routes.spec.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = "test";
 
-const chai = require( "chai" );
-const should = chai.should();
+const { expect } = require( "chai" );
+// const should = chai.should();
 const request = require( "supertest" );
 
 const server = require( "../server" );
@@ -9,13 +9,20 @@ const knex = require( "../config/bookshelf" )["knex"];
 
 describe( "API routes", function() {
   describe( "GET /api/v1/sightings", function() {
-    after(function () {
-      knex.destroy();
+
+    after( function (done) {
+      knex.destroy(done);
     });
-    it( "should return all sightings", function( done ) {
-      request( server )
-        .get( "/api/v1/sightings" )
-        .expect( 200, done );
+
+    it( "should return all sightings", async () => {
+      const response = await request( server ).get( "/api/v1/sightings" );
+      expect( response.statusCode ).to.equal( 200 );
+      expect( response.body ).to.be.an('array');
+      expect( response.body[0] ).to.be.an( 'object' );
+      expect( response.body[0].id ).to.be.a( 'number' );
+      expect( response.body[0].celebrity ).to.be.a( 'string' );
+      expect( response.body[0].celebrity ).to.equal( 'Rowan Atkinson' );
+      
     });
   });
 });
