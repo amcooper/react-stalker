d26ea815dcedf3423dd2ce244b2e413135b84c2b
Adam Cooper <theadamcooper@protonmail.com>
2018-09-10 09:54:32 -0400

    Added test structure and s'mo' tests

--- server/test/routes.spec.js
+++ server/test/routes.spec.js
@@ -23,7 +23,7 @@ describe( "API routes", function() {
       done();
     });
   });
-  
+
   after( function (done) {
     knex.destroy(done);
   });
@@ -46,6 +46,65 @@ describe( "API routes", function() {
       expect( response.body[0].location ).to.equal( 'Brooklyn' );
       expect( response.body[0].comment ).to.be.a( 'string' );
       expect( response.body[0].comment ).to.equal( 'Mr. Bean!' );
+      expect( response.body[1] ).to.be.an( 'object' );
+      expect( response.body[1].id ).to.be.a( 'number' );
+      expect( response.body[1].celebrity ).to.be.a( 'string' );
+      expect( response.body[1].celebrity ).to.equal( 'Allison Janney' );
+      expect( response.body[1].stalker ).to.be.a( 'string' );
+      expect( response.body[1].stalker ).to.equal( 'Aretha Cooper' );
+      expect( response.body[1].date ).to.be.a( 'string' );
+      // expect( response.body[1].date ).to.equal( Date(2018,3,21,11,59,59) );
+      expect( response.body[1].location ).to.be.a( 'string' );
+      expect( response.body[1].location ).to.equal( 'Philadelphia' );
+      expect( response.body[1].comment ).to.be.a( 'string' );
+      expect( response.body[1].comment ).to.equal( 'Congrats.' );
+      expect( response.body[2] ).to.be.an( 'object' );
+      expect( response.body[2].id ).to.be.a( 'number' );
+      expect( response.body[2].celebrity ).to.be.a( 'string' );
+      expect( response.body[2].celebrity ).to.equal( 'Mary J. Blige' );
+      expect( response.body[2].stalker ).to.be.a( 'string' );
+      expect( response.body[2].stalker ).to.equal( 'Alan Cooper' );
+      expect( response.body[2].date ).to.be.a( 'string' );
+      // expect( response.body[2].date ).to.equal( Date(2018,3,21,11,59,59) );
+      expect( response.body[2].location ).to.be.a( 'string' );
+      expect( response.body[2].location ).to.equal( 'Baltimore' );
+      expect( response.body[2].comment ).to.be.a( 'string' );
+      expect( response.body[2].comment ).to.equal( 'Extra foam' );
     });
+  });
+
+  describe( "GET /api/v1/sightings/1", function() {
+
+    it( "should return the sighting whose id is 1", async () => {
+      const response = await request( server ).get( "/api/v1/sightings/1" );
+      expect( response.statusCode ).to.equal( 200 );
+      expect( response.body ).to.be.an('object');
+      expect( response.body.id ).to.be.a( 'number' );
+      expect( response.body.celebrity ).to.be.a( 'string' );
+      expect( response.body.celebrity ).to.equal( 'Rowan Atkinson' );
+      expect( response.body.stalker ).to.be.a( 'string' );
+      expect( response.body.stalker ).to.equal( 'Adam Cooper' );
+      expect( response.body.date ).to.be.a( 'string' );
+      // expect( response.body.date ).to.equal( Date(2018,3,21,11,59,59) );
+      expect( response.body.location ).to.be.a( 'string' );
+      expect( response.body.location ).to.equal( 'Brooklyn' );
+      expect( response.body.comment ).to.be.a( 'string' );
+      expect( response.body.comment ).to.equal( 'Mr. Bean!' );      
+    });
+  });
+
+  describe( "POST /api/v1/sightings", function() {
+
+    it( "should add a new sighting", async () => {});
+  });
+
+  describe( "PUT /api/v1/sightings/1", function() {
+
+    it( "should change an existing sighting", async () => {});
+  });
+
+  describe( "DELETE /api/v1/sightings/1", function() {
+
+    it( "should remove an existing sighting", async () => {});
   });
 });
