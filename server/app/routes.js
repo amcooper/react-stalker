029b7fab604016b7b3fc5298c498f3e2fb84d894
Adam Cooper <theadamcooper@protonmail.com>
2018-09-12 10:15:32 -0400

    Hooked in ESLint and Prettier. Fix server/ issues.

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,45 +1,50 @@
-const Sighting = require( './models/sighting' );
-const express = require( 'express' );
+const Sighting = require("./models/sighting");
+const express = require("express");
 const router = express.Router();
 
-	router.get( '/sightings', ( request, response, next ) => {
-		Sighting.fetchAll()
-      .then( res => response.json( res ))
-      .catch( e => next( e ));
-	});
+router.get("/sightings", (request, response, next) => {
+  Sighting.fetchAll()
+    .then(res => response.json(res))
+    .catch(e => next(e));
+});
 
-  router.get( '/sightings/:id', (request, response, next ) => {
-    Sighting.where('id', request.params.id).fetch()
-      .then( res => response.json( res ))
-      .catch( e => next( e ));
-  });
+router.get("/sightings/:id", (request, response, next) => {
+  Sighting.where("id", request.params.id)
+    .fetch()
+    .then(res => response.json(res))
+    .catch(e => next(e));
+});
 
-  router.post( '/sightings', ( request, response, next ) => {
-    Sighting.forge( request.body )
-      .save()
-      .then( res => response.json( res ))
-      .catch( e => next( e ));
-  });
+router.post("/sightings", (request, response, next) => {
+  Sighting.forge(request.body)
+    .save()
+    .then(res => response.json(res))
+    .catch(e => next(e));
+});
 
-  router.put( '/sightings/:id', ( request, response, next ) => {
-    Sighting.where( 'id', request.params.id ).fetch()
-      .then( res_fetch =>
-        res_fetch.set( request.body )
+router.put("/sightings/:id", (request, response, next) => {
+  Sighting.where("id", request.params.id)
+    .fetch()
+    .then(res_fetch =>
+      res_fetch
+        .set(request.body)
         .save()
-        .then( res_save => response.json( res_save ))
-				.catch( e_save => next( e_save ))
-      )
-      .catch( e_fetch => next( e_fetch ));
-  });
+        .then(res_save => response.json(res_save))
+        .catch(e_save => next(e_save))
+    )
+    .catch(e_fetch => next(e_fetch));
+});
 
-  router.delete( '/sightings/:id', ( request, response, next ) => {
-    Sighting.where( 'id', request.params.id ).fetch()
-      .then( res_fetch =>
-        res_fetch.destroy()
-        .then( res_destroy => response.json( res_destroy ))
-        .catch( e_save => next( e_save ))
-      )
-      .catch( e_fetch => next( e_fetch ));
-  });
+router.delete("/sightings/:id", (request, response, next) => {
+  Sighting.where("id", request.params.id)
+    .fetch()
+    .then(res_fetch =>
+      res_fetch
+        .destroy()
+        .then(res_destroy => response.json(res_destroy))
+        .catch(e_save => next(e_save))
+    )
+    .catch(e_fetch => next(e_fetch));
+});
 
 module.exports = router;
