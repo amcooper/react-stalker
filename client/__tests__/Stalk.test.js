e811a0767805de27579917b6002b497f00740c78
Adam Cooper <amcooper@gmail.com>
2018-09-24 08:44:10 -0400

    Boo

--- client/__tests__/Stalk.test.js
+++ client/__tests__/Stalk.test.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Stalk from "../src/Stalk.js";
+import stalkList from "../fixtures/fixtures";
 
-const testStalk = {
-  id: 1,
-  celebrity: "Katherine Zellner",
-  stalker: "Terrell Devereaux",
-  location: "Indianapolis",
-  date: new Date("2018-09-20"),
-  comment: "Win win win!"
-};
-
-const { id, celebrity, stalker, location, date, comment } = testStalk;
+const { celebrity, stalker, location, date, comment } = stalkList[0];
+console.log(`***** `, stalker, celebrity, stalkList);
 
 describe("Stalk component", () => {
   // This test is rendered moot by the snapshot test!
-  xit("renders with data", () => {
-    const stalk = shallow(<Stalk item={testStalk} />);
+  it("renders with data", () => {
+    const stalk = shallow(<Stalk item={stalkList[0]} />);
+    console.log(`***** `, stalker, celebrity, stalkList);
     const stalkText = new RegExp(
       `<button>edit<\/button><button>delete<\/button>${stalker}\\s*spotted\\s*${celebrity}\\s*on\\s*${date.toDateString()}\\s*in\\s*${location}\\s*: &quot;${comment}`
     );
@@ -27,7 +21,7 @@ describe("Stalk component", () => {
     const editSpy = jest.fn();
     const deleteSpy = jest.fn();
     const stalk = shallow(
-      <Stalk item={testStalk} onEdit={editSpy} onDelete={deleteSpy} />
+      <Stalk item={stalkList[0]} onEdit={editSpy} onDelete={deleteSpy} />
     );
     const editButton = stalk.findWhere(
       n => n.type() === "button" && n.text() === "edit"
