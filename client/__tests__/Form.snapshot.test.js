aba81b02dbfb1630edf42c3e871ae8c6fd55ae27
Adam Cooper <amcooper@gmail.com>
2018-09-26 23:12:29 -0400

    ding

--- client/__tests__/Form.snapshot.test.js
+++ client/__tests__/Form.snapshot.test.js
@@ -6,6 +6,7 @@ import { stalkList } from "../fixtures/fixtures";
 
 describe("Form component renders the form correctly", () => {
   it("create form", () => {
+    Date.now = jest.fn(() => 1537963884615);
     const rendered = renderer.create(<Form isEditForm={false} item={null} />);
 
     expect(rendered.toJSON()).toMatchSnapshot();
@@ -13,7 +14,7 @@ describe("Form component renders the form correctly", () => {
 
   it("edit form", () => {
     const rendered = renderer.create(
-      <Form isEditForm={false} item={stalkList[0]} />
+      <Form isEditForm={true} item={stalkList[0]} />
     );
 
     expect(rendered.toJSON()).toMatchSnapshot();
