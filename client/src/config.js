2b23d8663aa7c8dafde8ba70e65d01b09a11e7a9
Adam Cooper <theadamcooper@protonmail.com>
2018-10-04 17:24:29 -0400

    quick fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiURL: {
     development: "http://localhost:3033/api/v1/sightings",
-    production: "https://react-stalker-api.herokuapp.com/api/v1/sightings"
+    production: "https://react-stalker.herokuapp.com/api/v1/sightings"
   }
 };
