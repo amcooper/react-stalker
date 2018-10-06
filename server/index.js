caadf516e95c357b6efa8680776817291a0be62f
amcooper <theadamcooper@protonmail.com>
2018-10-06 00:57:01 -0400

    Deleting

--- server/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./server");
-const port = process.env.PORT;
-
-app.listen(port, () => {
-  console.log(`A quokka is listening on port ${port}.`);
-  console.log(`${process.env.NODE_ENV} - ${process.env.PORT}`);
-});
