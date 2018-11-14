31e60e96892525fae15ccca6126818e4e0b2f35c
Adam Cooper <amcooper@gmail.com>
2018-11-14 07:52:00 -0500

    boop

--- client/jest.config.js
+++ client/jest.config.js
@@ -89,7 +89,7 @@ module.exports = {
   // notifyMode: "always",
 
   // A preset that is used as a base for Jest's configuration
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-babel",
 
   // Run tests from one or more projects
   // projects: null,
--- client/testSetup.js
+++ client/testSetup.js
@@ -2,5 +2,5 @@
 // import Adapter from "enzyme-adapter-react-15";
 const Enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-15");
-debugger;
+// debugger;
 Enzyme.configure({ adapter: new Adapter() });
